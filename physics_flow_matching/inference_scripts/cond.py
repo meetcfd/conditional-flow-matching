5cdfa42d21bd872579022bfacf8c624286092545
meet <mparikh2@nd.edu>
2024-12-18 04:33:21 -1000

    update cond and utils

--- physics_flow_matching/inference_scripts/cond.py
+++ physics_flow_matching/inference_scripts/cond.py
@@ -146,7 +146,60 @@ def infer_gradfree(fm : FlowMatcher, cfm_model : torch.nn.Module,
                 
                 else:
                     scaled_grad, _ = grad_cost_func(meas_func, x, conditioning, 
-                                                    is_grad_free=True, grad_free={"a_t" : a_t, "b_t" : b_t, "x_gauss" : x_gauss},
+                                                    is_grad_free=True, use_fd=False, grad_free={"a_t" : a_t, "b_t" : b_t, "x_gauss" : x_gauss},
+                                                    **kwargs)
+                    scaled_grad *=  torch.linalg.norm(v.flatten())
+                    v = v - conditioning_scale*beta*scaled_grad
+                    x = x_fixed + (v)*dt #
+                
+                x = x.detach()
+                      
+        samples.append(x.cpu().numpy())
+        
+    return np.concatenate(samples)
+
+def infer_grad_fd(fm : FlowMatcher, cfm_model : torch.nn.Module,
+                   samples_per_batch, total_samples, dims_of_img, 
+                   num_of_steps, grad_cost_func, meas_func, conditioning, conditioning_scale,
+                   device, refine, **kwargs):
+    """Use finite difference to approx the u(t,x)"""
+  
+    ts = torch.linspace(0, 1, num_of_steps, device=device)
+    dt = ts[1] - ts[0]
+    
+    samples_per_batch = 1
+    
+    samples = []
+    
+    for i in range(total_samples//samples_per_batch):
+        samples_size = samples_per_batch
+        if i == total_samples//samples_per_batch - 1:
+            samples_size = samples_per_batch + total_samples%samples_per_batch
+        samples_size = (samples_size,) + dims_of_img 
+        
+        x = torch.randn(samples_size, device=device)
+        first_step = True
+                
+        for t in tqdm(ts):
+            _, beta, a_t, b_t = fm.compute_lambda_and_beta(t)
+            beta, a_t, b_t = pad_t_like_x(beta, x).to(device),\
+                             pad_t_like_x(a_t, x).to(device), pad_t_like_x(b_t, x).to(device)
+
+            x_fixed = x.clone().detach()
+            
+            for _ in range(refine): ##Picard Iteration
+
+                x = x.requires_grad_()
+                v = cfm_model(t, x)
+                
+                if first_step:
+                    first_step=False
+                    x = x + v*dt
+                    break
+                
+                else:
+                    scaled_grad, _ = grad_cost_func(meas_func, x, conditioning, 
+                                                    is_grad_free=True, use_fd=True, grad_free={"x_prev" : x_fixed, "dt": dt, "t" : t},
                                                     **kwargs)
                     scaled_grad *=  torch.linalg.norm(v.flatten())
                     v = v - conditioning_scale*beta*scaled_grad
--- physics_flow_matching/inference_scripts/utils.py
+++ physics_flow_matching/inference_scripts/utils.py
@@ -5,20 +5,36 @@ def inpainting(x_hat, **kwargs):
     # mask = kwargs["mask"]
     return x_hat[..., slice_x, slice_y]#x_hat * mask ## has no effect on the conditioning
 
+def wall_pres_forward(x_hat, **kwargs):
+    c = kwargs['channel']
+    det_model = kwargs['model']
+    slice_x, slice_y = slice(kwargs["sx"],kwargs["ex"]), slice(kwargs["sy"],kwargs["ey"])
+    x_pred = det_model(x_hat)
+    return x_pred[..., c:c+1, slice_x, slice_y]*kwargs["meas_std"][:, c:c+1] + kwargs["meas_mean"][:, c:c+1]
+
 def cost_func(meas_func, x_hat, measurement, **kwargs):
     pred_measurement = meas_func(x_hat, **kwargs)
     diff = pred_measurement - measurement
-    return torch.linalg.norm(diff.flatten())
+    return (diff**2).mean()  #torch.linalg.norm(diff.flatten()) #
 
 def grad_cost_func(meas_func, x, measurement, **kwargs):
     
     if kwargs["is_grad_free"]:
-        a_t, b_t, x_gauss = kwargs["grad_free"]["a_t"], kwargs["grad_free"]["b_t"], kwargs["grad_free"]["x_gauss"]
-        x_hat = 1/a_t * (x - b_t * x_gauss)
+        
+        if kwargs["use_fd"]:
+            assert "x_prev" in kwargs["grad_free"].keys() and "dt" in kwargs["grad_free"].keys(), "previous step is not cached!"
+            x_prev, dt, t = kwargs["grad_free"]["x_prev"], kwargs["grad_free"]["dt"],  kwargs["grad_free"]["t"]
+            v_fd = (x - x_prev)/dt
+            x_hat = x + (1 - t)*v_fd
+        else:
+            a_t, b_t, x_gauss = kwargs["grad_free"]["a_t"], kwargs["grad_free"]["b_t"], kwargs["grad_free"]["x_gauss"]
+            x_hat = 1/a_t * (x - b_t * x_gauss)
     else:
         t, v = kwargs["grad"]["t"], kwargs["grad"]["v"]
         x_hat = x + (1 - t)*v
         
     diff_norm =  cost_func(meas_func, x_hat, measurement, **kwargs)
     
-    return torch.autograd.grad(diff_norm, x)[0], diff_norm.item()
+    grad = torch.autograd.grad(diff_norm, x)[0]
+    unit_grad = grad / torch.linalg.norm(grad)
+    return unit_grad, diff_norm #torch.autograd.grad(diff_norm, x)[0], diff_norm.item() # 
